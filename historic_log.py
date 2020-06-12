063139ba20d9e10825f29a911778c5bc38febd31
Wallisson <cmwallisson@gmail.com>
2020-06-12 12:23:56 -0300

    Update log

--- historic_log.py
+++ historic_log.py
@@ -9,7 +9,7 @@ from get_data import stations
 import plot
 import methods
 import flowsignatures
-#import geoprocessing
+import geoprocessing
 #import clustering
 
 """
@@ -17,8 +17,7 @@ STEP 1 - GET AND PLOT ALL FLOW STATIONS DATA
 """
 bhsf = gpd.read_file(r'shapefiles\BHSF_WGS1984.shp')
 brazil_flu = gpd.read_file(r'shapefiles\Brazil_FlowStations_WithData.shp') #File provided by the HidroData repository from github.com/wallissoncarvalho/HidroData
-#Selecting the stations within the São Francisco basin
-bhsf_flu = brazil_flu[brazil_flu.geometry.within(bhsf.geometry[0])]
+bhsf_flu = brazil_flu[brazil_flu.geometry.within(bhsf.geometry[0])] #Selecting the stations within the São Francisco basin
 observed_flows_consisted = stations(list(bhsf_flu['Code']),'3',only_consisted=True) #Downloading all consisted stations data
 observed_flows_consisted.to_pickle(r'data/all_observed_consisted_flows.pkl') #Saving all stations data
 
@@ -26,14 +25,15 @@ observed_flows_consisted.to_pickle(r'data/all_observed_consisted_flows.pkl') #Sa
 STEP 2 - REMOVING AFFECTED STATIONS BY RESERVOIRS
 """
 affected_stations = pd.read_pickle(r'data\affected_stations.pkl').melt()['value'].dropna().to_list()
-affected_stations = affected_stations + ['44950000'] #UNCONSISTED STATION
-observed_flows_consisted.drop(affected_stations,axis=1,inplace=True)
+affected_stations = affected_stations + ['44950000'] #Adding an inconsistent station to drop
+affected_stations = affected_stations + ['44740000'] # The station 44740000 is the same that 44090000
+observed_flows_consisted.drop(affected_stations,axis=1,inplace=True) #Drop selected stations data
 
 """
 STEP 3 - PLOTTING AVALIABLE DATA
 """
 plot.gantt(observed_flows_consisted,r'graphics/All Stations Data Avaibility Consisted') #Plotting all stations data avaiability
-asy = plot.available_stations_year(observed_flows_consisted)
+asy = plot.available_stations_year(observed_flows_consisted) #Plotting station availability per year
 
 """
 STEP 4 - FILTERING STATIONS
@@ -52,4 +52,41 @@ signatures.to_pickle(r'data/signatures.pkl')
 """
 STEP 6 - PHYSIOGRAFIC DATA
 """
+physiographic_data = geoprocessing.physiographic_data(r'shapefiles\Watersheds.shp',r'shapefiles\LongestFlowPath.shp',r'raster/mdt_23s.tif')
+physiographic_data.to_pickle(r'data/physiographic_data.pkl')
+
+"""
+STEP 7 - GENERATING AND #STANDARIZING ALL PARAMETERS
+"""
+
+"""
+STEP 8 - ANALYZING CORRELATIONS
+"""
+
+"""
+STEP 9 - CLUSTERING ASSESSMENT
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
